7f08e1174c4a32a976e64dc05ec660fda29c5894
Jacob Ramirez <jramirez@widercircle.com>
2022-07-26 14:58:20 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://media.giphy.com/media/fw9CvIUMFkUltLw6zS/giphy.gif"
+          alt="logo"
+        />
+        <p>Macaron Store</p>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
